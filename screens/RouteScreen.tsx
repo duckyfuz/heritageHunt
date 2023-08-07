0ac63b9c8d154e033702ce208de70a347d79ff42
duckyfuz <kenf@iMac&cheese.local>
2023-08-07 20:45:56 +0800

    Add map

--- screens/RouteScreen.tsx
+++ screens/RouteScreen.tsx
@@ -1,12 +1,31 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "react-native-paper";
+import MapView from "react-native-maps";
+import { View, StyleSheet } from "react-native";
 
 const RouteScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>RouteScreen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
   );
 };
 
 export default RouteScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
