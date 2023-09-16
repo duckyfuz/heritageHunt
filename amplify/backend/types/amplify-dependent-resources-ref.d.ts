5459d3554aa6538c0e1551d2f5d01ad8386cb98e
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-09-17 00:09:19 +0800

    Add AWS Amplify for auth & GraphQL)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "heritagehunt": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "heritagehunt6453aa05": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
